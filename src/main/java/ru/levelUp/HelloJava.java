057647dab1cd0e6b23e3a799f91092bb021189e5
Krasnova.EA <krasnovaea95@yandex.ru>
2018-04-11 20:22:07 +0300

    init new commit

--- src/main/java/ru/levelUp/HelloJava.java
+++ src/main/java/ru/levelUp/HelloJava.java
@@ -3,5 +3,6 @@ package ru.levelUp;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("I love Java!");
+        System.out.println("I am Java Developer");
     }
 }
